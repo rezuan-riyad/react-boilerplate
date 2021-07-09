98cc56770ec157531c7251266f993f6803544b9d
Rezuan <rezuan@pop-os.localdomain>
2021-07-09 22:17:23 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/app.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>React Boilerplate</h1>
+      <ul>
+        <li>Configured with webpack 5</li>
+        <li>react, react-dom, redux, react-redux, redux-thunk, axios, materialize-css installed.</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
